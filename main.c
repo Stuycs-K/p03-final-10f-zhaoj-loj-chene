2c913f309bf623f8ae15e0fe0c7ffdf30da28551
ElizabethChen <elizabethchen1227@gmail.com>
2026-01-08 15:31:24 -0500

    using mpg123 to play mp3 files test

--- main.c
+++ main.c
@@ -2,16 +2,24 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
-#include <sys/wait.h>
 #include <fcntl.h>
-#include <mpg123.h>
 #include <errno.h>
 
 int err(){
     printf("errno %d\n",errno);
     printf("%s\n",strerror(errno));
+    return 0;
 }
 
-void test(){
-  execvp(--listentry, -1 )
+int test(){
+  char path[256] = "whoosh.mp3";
+  char *args[] = {"mpg123", path, NULL};
+  if (execvp(args[0], args) < 0){
+    err();
+  }
+  return 0;
+}
+
+void main(){
+  test();
 }
