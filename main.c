82a884fd67572b005a195d525faaf428640755ea
jzhao60 <jasonzhao164@gmail.com>
2026-01-09 13:17:17 -0500

    running mpg123 externally works

--- main.c
+++ main.c
@@ -1,12 +1,45 @@
-#include "mpg123-1.33.4/src/mpg123.c"
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <signal.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+pid_t mpg123_pid = -1;
+
+void cleanup(int sig) {
+    if (mpg123_pid > 0) {
+        kill(mpg123_pid, SIGTERM);
+        waitpid(mpg123_pid, NULL, 0);
+    }
+    printf("\nPlayback stopped.\n");
+    exit(0);
+}
 
 int main(int argc, char *argv[]) {
-    mpg123_init();
-    mpg123_handle *mh = mpg123_new(NULL, NULL);
-    mpg123_open(mh, "male-death-scream-horror-352706.mp3");
-    mpg123_play(mh);
-    mpg123_delete(mh);
-    mpg123_exit();
+    if (argc < 2) {
+        printf("Usage: %s <mp3_file>\n", argv[0]);
+        return 1;
+    }
+
+    signal(SIGINT, cleanup);
+
+    mpg123_pid = fork();
+
+    if (mpg123_pid == -1) {
+        perror("fork");
+        return 1;
+    }
+
+    if (mpg123_pid == 0) {
+        execlp("mpg123", "mpg123", argv[1], NULL);
+
+        perror("Failed to run mpg123 - make sure it's installed (sudo apt-get install mpg123)");
+        exit(1);
+    }
+    int status;
+    waitpid(mpg123_pid, &status, 0);
+
+    printf("Playback finished.\n");
     return 0;
 }
