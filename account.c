42d01cee11034984d066fbc890c127093a8f794f
jlo176 <jaydenl176@nycstudents.net>
2026-01-09 15:32:07 -0500

    write create user function

--- account.c
+++ account.c
@@ -13,8 +13,25 @@ int error(){
     exit(1);
 }
 
-int createuser(char* username, char* password){
-  return 0;
+int createuser(char* username, char* password){ //returns 0 if username taken, 1 otherwise
+  int f = open("users.dat", O_RDWR | O_APPEND, 0);
+  if (f < 0){
+    error();
+  }
+  char buff[100];
+  while(read(f, buff, 100) == 100){
+    if (!strcmp(buff, username)){
+      return 0;
+    }
+    lseek(f, sizeof(struct user) - 100, SEEK_CUR);
+  }
+  struct user* new_acc = malloc(sizeof(struct user));
+  new_acc->username = username;
+  new_acc->password = password;
+  if (write(f, new_acc, sizeof(struct user)) < 0){
+    error();
+  }
+  return 1;
 }
 
 int login(char* username, char* password, struct user* u_ptr){ //returns 0 if user not found, 1 otherwise
