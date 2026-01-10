2c2bff631dfc5d6a9bd4567f5ca90d5b7ae550d2
jlo176 <jaydenl176@nycstudents.net>
2026-01-09 19:21:06 -0500

    fix return type of user prompt function

--- account.h
+++ account.h
@@ -3,7 +3,7 @@
   struct playlist {char name[100]; char songs[50][100];};
   struct user {char username[100]; char password[100]; struct playlist user_playlists[5]};
   int error();
-  int user_prompt();
+  void user_prompt();
   int createuser(char* username, char* password);
   int login(char* username, char* password, struct user* u_ptr);
 #endif
