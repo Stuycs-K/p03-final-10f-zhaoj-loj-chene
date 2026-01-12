063c1409ea5ee89e28e508945f4cbc3f454899e2
jzhao60 <jasonzhao164@gmail.com>
2026-01-12 02:58:12 -0500

    merged server

--- server.c
+++ server.c
@@ -1,31 +1,4 @@
 #include "networking.h"
-<<<<<<< HEAD
-
-void subserver_logic(int client_socket){
-  char buff[200];
-  int bytes = recv(client_socket, buff, 200, 0);
-  if (bytes <= 0){
-    printf("socket closed\n");
-    exit(1);
-  }
-  buff[bytes] = '\0';
-  printf("received %s", buff);
-
-  for (int i = 0; buff[i] != '\0'; i++){
-    if (isupper(buff[i])){
-      buff[i] = (buff[i] - 'A' + 13) % 26 + 'A';
-    }
-    else if (islower(buff[i])){
-      buff[i] = (buff[i] - 'a' + 13) % 26 + 'a';
-    }
-  }
-
-  if (send(client_socket, buff, 200, 0) < 0){
-    perror("send");
-    exit(1);
-  }
-  printf("sent %s", buff);
-=======
 #include "account.h"
 
 int parse(char * line, char * delim, char ** arg_ary){
@@ -163,23 +136,10 @@ void subserver_logic(int client_socket){
       fflush(stdout);
     }
   }
->>>>>>> main
 }
 
 int main(int argc, char *argv[] ) {
   int listen_socket = server_setup();
-<<<<<<< HEAD
-  while(1){
-    int client_socket = server_tcp_handshake(listen_socket);
-    int pid = fork();
-    if (pid == 0){
-      while(1){
-        subserver_logic(client_socket);
-      }
-    }
-  }
-
-=======
 
   while(1){
     int client_socket = server_tcp_handshake(listen_socket);
@@ -193,5 +153,4 @@ int main(int argc, char *argv[] ) {
     }
     close(client_socket);
   }
->>>>>>> main
 }
