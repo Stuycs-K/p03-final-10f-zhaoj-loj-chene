a0ed204ab7fde48090c9a19763b8b3655354b18f
ElizabethChen <elizabethchen1227@gmail.com>
2026-01-11 22:52:29 -0500

    fix client logic

--- client.c
+++ client.c
@@ -1,45 +1,32 @@
 #include "networking.h"
 
-//reading user input
-void read_input(char* buffer, int size) {
-  if (fgets(buffer, size, stdin) == NULL) {
-    exit(0);
-  }
-
-  int len = strlen(buffer);
-  if (len > 0 && buffer[len - 1] == '\n') {
-    buffer[len - 1] = '\0';
-  }
-}
-
-void clientLogic(int server_socket){
-  char buffer[BUFFER_SIZE];
-
-  printf("Enter a message: \n");
-  if(fgets(buffer, BUFFER_SIZE, stdin) == NULL){
-      exit(1);
-  }
-  sscanf(buffer, "%[^\n]", buffer);
-
-  int s = send(server_socket, buffer, strlen(buffer), 0);
-  err(s, "send to client error\n");
-
-  int bytes = recv(server_socket, buffer, BUFFER_SIZE - 1, 0);
-  buffer[bytes] = '\0';
-  err(bytes, "error receiving\n");
-
-  printf("server response: %s\n", buffer);
-}
-
 int main(int argc, char *argv[] ) {
-  while(1){
-    char* IP = "127.0.0.1";
-    if(argc>1){
-      IP=argv[1];
+  char* IP = "127.0.0.1";
+  if(argc>1){
+    IP=argv[1];
+  }
+  int server_socket = client_tcp_handshake(IP);
+  printf("connected to music server.\n");
+
+  int f = fork();
+  if(f == 0){
+    char buffer[BUFFER_SIZE];
+    while(1){
+      int bytes = recv(server_socket, buffer, BUFFER_SIZE - 1, 0);
+      err(bytes, "server disconnected");
+      buffer[bytes] = '\0';
+      printf("%s", buffer);
+      fflush(stdout);
     }
-    int server_socket = client_tcp_handshake(IP);
-    printf("client connected.\n");
-    clientLogic(server_socket);
+    kill(getppid(), SIGKILL);
+    exit(0);
+  } else {
+    char buffer[BUFFER_SIZE];
+    while(1){
+      if (fgets(buffer, BUFFER_SIZE, stdin) == NULL) break;
+      send(server_socket, buffer, strlen(buffer), 0);
+    }
     close(server_socket);
   }
+  return 0;
 }
