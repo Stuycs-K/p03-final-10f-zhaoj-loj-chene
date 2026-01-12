aa376b0f3e0bd33ba30e09e9785b56a712798fb6
ElizabethChen <elizabethchen1227@gmail.com>
2026-01-11 22:55:04 -0500

    update account.h (removed void user_prompt(struct user* u_ptr);)

--- account.h
+++ account.h
@@ -3,7 +3,6 @@
   struct playlist {char name[100]; char songs[50][100];};
   struct user {char username[100]; char password[100]; struct playlist user_playlists[5];};
   int error();
-  void user_prompt(struct user* u_ptr);
   int createuser(char* username, char* password);
   int login(char* username, char* password, struct user* u_ptr);
 #endif
