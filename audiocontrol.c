f02fe567fbdea8b8eaae465745b485daab45e4da
Jason <jasonz164@nycstudents.net>
2026-01-12 15:35:36 -0500

    audio control close, doesnt seem to be accessing the mp3s

--- audiocontrol.c
+++ audiocontrol.c
@@ -34,17 +34,91 @@ void play_with_volume(int percent) {
         char scale_arg[16];
         snprintf(scale_arg, sizeof(scale_arg), "%d", scale);
 
-        execlp(
-            "mpg123",
-            "mpg123",
-            "-f", scale_arg,
-            mp3_file,
-            NULL
-        );
+        execlp("mpg123", "mpg123", "-f", scale_arg, mp3_file, NULL);
 
         perror("mpg123 failed");
         exit(1);
     }
+}
+
+void mp3_prompt(){
+  if (argc < 2) {
+      printf("Usage: %s mp3_file\n", argv[0]);
+      return 1;
+  }
+
+  mp3_file = argv[1];
+  char path[256] = "./music/";
+  strcat(path, mp3_file);
+  printf("trying to open %s \n", path);
+  FILE *file = fopen(path, "rb");
+  if(!file){
+    printf("error: song not found\n");
+    fflush(stdout);
+  }
+
+  while(1){
+    printf("create account or login? (type crt/lgn): ");
+    fflush(stdout);
+
+    if (fgets(input, sizeof(input), stdin)){
+      input[strcspn(input, "\n")] = '\0'; //trim \n at end
+      if (strcmp("crt", input) == 0){
+        while(1){
+          char username[100];
+          char password[100];
+          printf("enter username: ");
+          fflush(stdout);
+          if (!fgets(username, sizeof(username), stdin)){
+            error();
+          }
+          printf("enter password: ");
+          fflush(stdout);
+          if (!fgets(password, sizeof(password), stdin)){
+            error();
+          }
+          username[strcspn(username, "\n")] = '\0';
+          password[strcspn(password, "\n")] = '\0';
+          if (createuser(username, password)){
+            break;
+          }
+          printf("username taken\n");
+          fflush(stdout);
+        }
+      }
+      else if (strcmp("lgn", input) == 0){
+        while(1){
+          char username[100];
+          char password[100];
+          printf("enter username: ");
+          fflush(stdout);
+          if (!fgets(username, sizeof(username), stdin)){
+            error();
+          }
+          printf("enter password: ");
+          fflush(stdout);
+          if (!fgets(password, sizeof(password), stdin)){
+            error();
+          }
+          username[strcspn(username, "\n")] = '\0';
+          password[strcspn(password, "\n")] = '\0';
+          if (login(username, password, u_ptr)){
+            break;
+          }
+          printf("wrong username or password\n");
+          fflush(stdout);
+        }
+        break;
+      }
+      else{
+        printf("invalid input: enter \"crt\" or \"lgn\"\n");
+        fflush(stdout);
+      }
+    }
+    else{
+      error();
+    }
+  }
 }
 
 int main(int argc, char *argv[]) {
