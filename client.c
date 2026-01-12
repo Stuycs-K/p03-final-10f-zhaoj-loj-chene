4edd59022688fbb18a74e8fc9c2c6754b61ccb25
jzhao60 <jasonzhao164@gmail.com>
2026-01-12 02:59:25 -0500

    client merged, to be tested

--- client.c
+++ client.c
@@ -1,40 +1,11 @@
 #include "networking.h"
 
-<<<<<<< HEAD
-void clientLogic(int server_socket){
-  char buff[200];
-  printf("input a string:\n");
-  if (!fgets(buff, 200, stdin)){
-    printf("client closed\n");
-    exit(1);
-  }
-
-  if (send(server_socket, buff, 200, 0) < 0){
-    perror("send");
-    exit(1);
-  }
-
-  if (recv(server_socket, buff, 200, 0) < 0){
-    perror("recv");
-    exit(1);
-  }
-  printf("string rot13: %s", buff);
-}
-
-=======
->>>>>>> main
 int main(int argc, char *argv[] ) {
   char* IP = "127.0.0.1";
   if(argc>1){
     IP=argv[1];
   }
   int server_socket = client_tcp_handshake(IP);
-<<<<<<< HEAD
-  printf("client connected.\n");
-  while(1){
-    clientLogic(server_socket);
-  }
-=======
   printf("connected to music server.\n");
 
   int f = fork();
@@ -58,5 +29,4 @@ int main(int argc, char *argv[] ) {
     close(server_socket);
   }
   return 0;
->>>>>>> main
 }
